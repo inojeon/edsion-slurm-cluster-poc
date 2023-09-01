3e869a61b09f583d5d42c3fafc9fb9f1c7036e35
ino <ino@kakao.com>
2023-09-01 15:25:37 +0900

    add front and backend

--- api/docker-entrypoint.sh
+++ api/docker-entrypoint.sh
@@ -2,4 +2,14 @@
 
 sudo service munge start
 
+
+/bin/bash
+
+
+cd /server/slurm-api/src
+sudo gunicorn --bind 0:8000 app.main:app --worker-class uvicorn.workers.UvicornWorker --daemon
+
+cd /server/edison-workbench
+sudo pnpm dev
+
 tail -f /dev/null
--- node/docker-entrypoint.sh
+++ node/docker-entrypoint.sh
@@ -5,4 +5,7 @@ sudo sed -i "s/REPLACE_IT/CPUs=$(nproc)/g" /etc/slurm-llnl/slurm.conf
 sudo service munge start
 sudo slurmd -N $(hostname)
 
+cp -R /tmp/repository/ /home/admin/repository/
+mkdir /home/admin/jobs 
+
 tail -f /dev/null
